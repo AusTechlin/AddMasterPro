298562cc84f9d0cebf1334087803da02a6369243
Austin Techlin <techlinaustin@gmail.com>
2024-09-11 16:47:28 -0700

    Added AppMasterPro

---
+++ AddMasterPro/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace AddMasterPro;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "AddMasterPro";
+    }
+
+
+    private void Clear_OnClicked(object sender, EventArgs e)
+    {
+        txtFirstNumber.Text = string.Empty;
+        txtSecondNumber.Text = string.Empty;
+        lblDisplay.Text = "0";
+    }
+
+    private void Add_OnClicked(object sender, EventArgs e)
+    {
+        Double dblFirst, dblSecond;
+        Double.TryParse(txtFirstNumber.Text, out dblFirst);
+        Double.TryParse(txtSecondNumber.Text, out dblSecond);
+
+        lblDisplay.Text = (dblFirst + dblSecond).ToString();
+    }
+}
